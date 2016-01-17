508abab3e0195a965fab737a109b300a3b5e939c
Vladimir Smagin <21h@blindage.org>
2016-01-17 06:03:15 +0600

    gqrx to sdr sharp converter

---
+++ gqrx2sdrsharp.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+fileGQRX='gqrx/bookmarks.csv'
+fileOUT='sdr#/frequencies.xml'
+
+header='<?xml version="1.0"?><ArrayOfMemoryEntry xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">'
+footer='</ArrayOfMemoryEntry>'
+
+def template(data):
+    i=0
+    for m in data:
+        data[i] = m.strip()
+        i += 1
+    resultString="<MemoryEntry><IsFavourite>false</IsFavourite><Name>{0[1]}</Name><GroupName>{0[4]}</GroupName><Frequency>{0[0]}</Frequency><DetectorType>{0[2]}</DetectorType><Shift>0</Shift><FilterBandwidth>{0[3]}</FilterBandwidth></MemoryEntry>".format(data)
+    return resultString
+
+fIN = open(fileGQRX, 'r+')
+fOUT = open(fileOUT, 'w')
+
+#пропустить первую таблицу с тегами
+for line in fIN:
+    if line=="\n": break
+fIN.readline() #строка с заголовками полей - пропустить
+#таблица частот
+fOUT.write(header)
+for line in fIN:
+    data = line.split(';')
+    dataFreq=data[0].strip()
+    dataName=data[1].strip()
+    dataMod=data[2].strip()
+    dataFilter=data[3].strip()
+    dataTag=data[4].strip()
+    print(template(data))
+    fOUT.write(template(data))
+fOUT.write(footer)
